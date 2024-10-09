2d7a446259976cf1c80f91646bb91854e1141e3c
Hershberger <02hayden.hershberger@gulllakecs.org>
2024-10-09 08:26:59 -0400

    Added Color

--- CastleBravo.py
+++ CastleBravo.py
@@ -2,8 +2,12 @@
 import sys  # sys module provides access to system-specific parameters and functions
 import time  # time module provides time-related functions (e.g., sleep)
 
-# Printing a welcome message to the console
-print("\nWelcome to InfoTechCenter V1.0\m")  # \m seems like a typo; it should probably be \n for a new line
+# ANSI escape code for blue text
+BLUE = '\033[94m'  # Bright blue
+RESET = '\033[0m'  # Reset to default color
+
+# Printing a welcome message to the console in blue
+print(BLUE + "\nWelcome to InfoTechCenter V1.0" + RESET)  # Use ANSI code for blue text
 
 # Initializing variables
 x = 0  # x is used as a counter to control the loop iterations
@@ -12,7 +16,7 @@ ellipsis = 0  # ellipsis controls the number of dots to display in the boot mess
 # A while loop that simulates the booting system
 while x != 20:  # Loop will run until x equals 20 (20 iterations)
     x += 1  # Increment the counter with each iteration
-    message = ("Infotech Center System Booting" + "." * ellipsis)  # Create the boot message with increasing dots
+    message = (BLUE + "Infotech Center System Booting" + "." * ellipsis + RESET)  # Create the boot message with increasing dots
     ellipsis += 1  # Increase the number of dots for each iteration
     sys.stdout.write("\r" + message)  # Overwrite the line in the console with the new message
     time.sleep(0.5)  # Pause the loop for 0.5 seconds to create a loading effect
@@ -23,4 +27,5 @@ while x != 20:  # Loop will run until x equals 20 (20 iterations)
 
     # When the counter reaches 20, the system boot message is complete
     if x == 20:
-        print("\n\nOperating System Booted up - Retina Scanned - Access Granted")  # Final message after boot completes
+        print("\n\n" + BLUE + "Operating System Booted up - Retina Scanned - Access Granted" + RESET)  # Final message after boot completes
+
