a0fee5eb3992b3a933b2fc07331720a7e7b9f71b
Hershberger <02hayden.hershberger@gulllakecs.org>
2024-10-15 08:08:21 -0400

    Added more else/elif

--- CastleBravo.py
+++ CastleBravo.py
@@ -24,5 +24,24 @@ def VRS():
               " of the forecast of", WeatherAlert, "weather conditions.")
         sleep(1)
         print("\nVRS system has been engaged only allowing you to drive 45mph")
+    elif WeatherAlert=="rainy":
+        print("\nThe National Weather Service has updated our alarm by 15 minutes because"
+              " of the forecast of", WeatherAlert, "weather conditions.")
+        sleep(1)
+        print("\nVRS system has been engaged only allowing you to drive 65mph")
+    elif WeatherAlert=="windy":
+        print("\nThe National Weather Service has updated our alarm by 5 minutes because"
+              " of the forecast of", WeatherAlert, "weather conditions.")
+        sleep(1)
+        print("\nVRS system has been engaged only allowing you to drive 70mph")
+    elif WeatherAlert=="icy":
+        print("\nThe National Weather Service has updated our alarm by 50 minutes because"
+              " of the forecast of", WeatherAlert, "weather conditions.")
+        sleep(1)
+        print("\nVRS system has been engaged only allowing you to drive 30mph")
+    else:
+        print("\nThe NWS is calling for", WeatherAlert, "skies. Drive carefully to get to your destination")
+        sleep(1)
+        print("\nVRS system has been disengaged")
 
 print(VRS())
