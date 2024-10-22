eeb9ef4117da346d01cc40e0330a317d54c7e73a
Hershberger <02hayden.hershberger@gulllakecs.org>
2024-10-22 08:14:32 -0400

    Added elifs to gasLevelAlert

--- CastleBravo.py
+++ CastleBravo.py
@@ -1,6 +1,6 @@
 print("\n**************************************************\n")
 
-print("Gasoline Branch")
+print("Gasoline Branch\n")
 
 import random
 from time import sleep
@@ -13,4 +13,26 @@ def gasStations():
     gasStationsList=["Vp","Shell","Meijer","Sam's Club","Marathon","Mobile","Speedway","Circle K"]
     return random.choice(gasStationsList)
 
-#def gasLevelAlert():
+def gasLevelAlert():
+    milesToGasStationLow=round(random.uniform(1,25),1)
+    milesToGasStationQuarterTank=round(random.uniform(25.1,50),1)
+    gasLevelIndicator = gasLevelGauge()
+    if gasLevelIndicator=="Empty":
+        print("***WARNING - YOU ARE ON EMPTY***\n")
+        sleep(2)
+        print("Calling Triple AAA")
+    elif gasLevelIndicator=="Low":
+        print("Your gas tank is on Low: Check GPS for closest gas station")
+        sleep(2)
+        print("The closest gas station is",gasStations(),"in",milesToGasStationLow,"miles")
+    elif gasLevelIndicator=="Quarter Tank":
+        print("Your gas tank is on Quarter Tank: Check GPS for closest gas station")
+        sleep(2)
+        print("The closest gas station is", gasStations(), "in", milesToGasStationQuarterTank, "miles")
+    elif gasLevelIndicator=="Half Tank":
+        print("Your gas tank is on Half Tank: Please enjoy your ride")
+    elif gasLevelIndicator == "Three Quarter Tank":
+        print("Your gas tank is on Three Quarter Tank: Please enjoy your ride")
+    else:
+        print("Your gas tank is on Full: Please enjoy your ride")
+print(gasLevelAlert())
